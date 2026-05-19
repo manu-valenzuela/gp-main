05de1eb0a1b93a07f74d3bf11fe55b7724999c1e
manu-valenzuela <mpval@outlook.com>
2026-05-19 18:04:04 -0400

    Add registroPersonas controller

--- gp-registroPersonas/src/main/java/com/gp/registroPersonas/controller/PersonaController.java
+++ gp-registroPersonas/src/main/java/com/gp/registroPersonas/controller/PersonaController.java
@@ -1,4 +1,25 @@
 package com.gp.registroPersonas.controller;
 
+import com.gp.registroPersonas.dto.PersonaResponseDto;
+import com.gp.registroPersonas.service.PersonaService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/personas")
 public class PersonaController {
+
+    private final PersonaService service;
+
+    public PersonaController(PersonaService service) { this.service = service; }
+
+    @GetMapping
+    public List<PersonaResponseDto> findAll() { return service.findAll(); }
+
+    @GetMapping("/{rut}")
+    public PersonaResponseDto findById(@PathVariable String rut) { return service.findById(rut); }
 }
