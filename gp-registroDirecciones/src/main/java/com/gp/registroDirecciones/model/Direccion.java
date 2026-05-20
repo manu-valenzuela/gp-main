139d1ba36bdb7ce65d67d988125485698ac9c441
manu-valenzuela <mpval@outlook.com>
2026-05-19 21:53:43 -0400

    Add 'direccion' model

--- gp-registroDirecciones/src/main/java/com/gp/registroDirecciones/model/Direccion.java
+++ gp-registroDirecciones/src/main/java/com/gp/registroDirecciones/model/Direccion.java
@@ -1,4 +1,27 @@
 package com.gp.registroDirecciones.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class Direccion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_direccion")
+    private Long id;
+    private String calle;
+    private Long numero;
+
+    @ManyToOne
+    @JoinColumn(name = "id_comuna")
+    private Comuna comuna;
+
+    @ManyToOne
+    @JoinColumn(name = "id_region")
+    private Region region;
+
 }
